27d104c48bc0fc4a3ef16d102ea9ddcb7df8ba90
Arhises <roma1039@gmail.com>
2025-07-12 08:21:30 +0300

    Analitics added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,28 @@ import ai.AiSurveyGenerator;
 import ai.ChatGptClient;
 import model.Survey;
 import model.User;
+import view.SurveyFrame;
+
+import javax.swing.*;
+import java.util.Random;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Cleaning memory... " + ChatGptClient.postClear());
-        System.out.println("balance: " + ChatGptClient.getBalance() + " calls left");
 
-        Survey survey = AiSurveyGenerator.generateSurvey("Capitals",new User(1,"Garik"),5);
-        System.out.println(survey);
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 800;
+
+    public static void main(String[] args) {
+//        System.out.println("Cleaning memory... " + ChatGptClient.postClear());
+        System.out.println("balance: " + ChatGptClient.getBalance() + " calls left");
+//
+
+
+        JFrame window = new SurveyFrame();
+        window.setVisible(true);
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
     }
 }
