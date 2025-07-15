136f51daa46cf5eaa4e051b4fa9dc44773317034
BeniRb <beni.ribakov@gmail.com>
2025-07-15 21:58:06 +0300

    added to user class+checked if works in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,24 @@ public class Main {
         System.out.println("Cleaning memory... " + ChatGptClient.postClear());
         System.out.println("balance: " + ChatGptClient.getBalance() + " calls left");
 
-        Survey survey = AiSurveyGenerator.generateSurvey("Capitals",new User(1,"Garik"),5);
+        Survey survey = AiSurveyGenerator.generateSurvey("Capitals", new User(1, "Garik"), 5);
         System.out.println(survey);
+
+
+        // check all users
+//        User.getOrCreateUser(1902,"beni");
+//        System.out.println("All users:");
+//        for (User user : User.getAllUsers()) {
+//            System.out.println(user);
+//        }
+//
+//        // check specific user
+//        User check = User.getUserById(1);
+//        if (check != null) {
+//            System.out.println("User with ID 1 exists: " + check.getUsername());
+//        } else {
+//            System.out.println("User with ID 1 not found.");
+//        }
+//    }
     }
 }
--- src/main/java/util/Keys.java
+++ src/main/java/util/Keys.java
@@ -5,9 +5,9 @@ public class Keys {
     //    each one adds his own :)
     //    Create this class in Util(this package)
 
-    //    public class MyKeys {
-    //      public static final String MY_ID = "YOUR ID";
-    //    }
+//        public class MyKeys {
+//          public static final String MY_ID = "YOUR ID";
+//        }
 
     //    AND check if is added to .gitignore like this:
     //    src/main/java/util/MyKeys.java
