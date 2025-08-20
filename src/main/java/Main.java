005ddfc72fa965699ad158a124fa25b42f019ec9
dalia <daliahen8@gmail.com>
2025-08-20 21:26:44 +0300

    telegram bot stuff added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,11 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Cleaning memory... " + ChatGptClient.postClear());
+        System.out.println("balance: " + ChatGptClient.getBalance() + " calls left");
+
+        Survey survey = AiSurveyGenerator.generateSurvey("Capitals", new User(1, "Garik"), 5);
+        System.out.println(survey);
 
         TelegramBotsApi telegramBotsApi = null;
         try {
