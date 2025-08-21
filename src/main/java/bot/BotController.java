71623613dc23184ea5e6ad0d74292994e0ba1e4c
dalia <daliahen8@gmail.com>
2025-08-21 22:30:08 +0300

    added user to Survey , finished half work

--- src/main/java/bot/BotController.java
+++ src/main/java/bot/BotController.java
@@ -2,16 +2,15 @@ package bot;
 
 import model.Survey;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class BotController {
 
     private Survey survey; //מבחינתי יש כבר שאלות ותשובות מה שצריך לעשות זה למלא בתשובות את המשתשמים עבור כל שאלה
 
 
+    public void handleUpdate(Update update){
 
+    }
 
-
-//    TODO: Responsibilities:
-//      - Handle /start, “Hi” → register user and broadcast welcome.
-//      - Route survey dispatch and incoming answer callbacks.
 }
