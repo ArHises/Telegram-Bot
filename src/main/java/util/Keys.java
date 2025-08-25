4d972f52365c078877bed0a8b100374298192f23
BeniRb <beni.ribakov@gmail.com>
2025-08-25 23:31:32 +0300

    changed data

--- src/main/java/util/Keys.java
+++ src/main/java/util/Keys.java
@@ -5,13 +5,13 @@ public class Keys {
     //    each one adds his own :)
     //    Create this class in Util(this package)
 
-        public class MyKeys {
-          public static final String MY_ID = "209299718";
-        }
+//        public class MyKeys {
+//          public static final String MY_ID = "your id";
+//        }
 
     //    AND check if is added to .gitignore like this:
     //    src/main/java/util/MyKeys.java
-    public static final String STUDENT_ID = MyKeys.MY_ID; // each one adds his own :)
-    public static final String MY_BOT_TOKEN  = "8182175157:AAGrT4vi06KHf2G2IboEMGF2-sjayUB6Dj0";
-    public static final String MY_USERNAME   = "@Survey121_bot";
+//    public static final String STUDENT_ID = MyKeys.MY_ID; // each one adds his own :)
+//    public static final String MY_BOT_TOKEN  = "123456:ABC...";
+//    public static final String MY_USERNAME   = "your_bot_username";
 }
